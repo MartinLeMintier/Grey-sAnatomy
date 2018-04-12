02bf0905da67678408778a7c138a616822db1a9e
CorentinBlanc <CorentinBlanc@users.noreply.github.com>
2018-04-12 13:28:44 +0200

    Accueil Graphique

--- Projet/src/projet/FenetreAcceuil.java
+++ Projet/src/projet/FenetreAcceuil.java
@@ -5,15 +5,10 @@
  */
 package projet;
 
-import javax.swing.BoxLayout;
-import javax.swing.Box;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
 import java.awt.event.*;
 import Controleur.Connexion;
+import java.awt.Color;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,19 +19,16 @@ import java.util.logging.Logger;
  */
 public class FenetreAcceuil extends JFrame implements ActionListener 
 {
-    public JPanel P1;
-    public JLabel l2 ;
-    public JTextField t2;
-    public JPanel P2 ;
-    public  JLabel l3;
-    public JTextField t3;
-    public JPanel P3 ;
-   public JLabel l4;
-   public JTextField t4;
-   public  JButton b4;
-   public  JPanel P4;
-   public Box Box1;
-   public Connexion conni;
+   protected JPanel P1, P2, P3, P4, P5, Ptitre;
+   protected JLabel l2, l3, l4, titre;
+    
+   protected JTextField t2, t3;
+    
+   protected JPasswordField t4;
+    
+   protected JButton b4; 
+   protected Box Box1;
+   protected Connexion conni;
    
    
     public FenetreAcceuil(){
@@ -50,47 +42,60 @@ public class FenetreAcceuil extends JFrame implements ActionListener
 
     //on part sur un nouveau pannel
     P1 = new JPanel();
+    P1.setBackground(new Color(0x97DFC6));
     P1.setLayout(new BoxLayout(P1, BoxLayout.LINE_AXIS));
     
     
     //on créé les différents composant du pannel
     l2 = new JLabel("A quelle BDD voulez vous vous connecter ?");
-    t2 = new JTextField(20);  
+    t2 = new JTextField(15);  
     
     //On les ajoute au pannel
     P2 = new JPanel();
+    P2.setBackground(new Color(0x97DFC6));
     P2.add(l2);
     P2.add(t2);
     
     
     //on créé les différents composant du pannel
     l3 = new JLabel("Quel est votre login ?");
-    t3 = new JTextField(20);
+    t3 = new JTextField(15);
     
     //On les ajoute au pannel//On les ajoute au pannel
     P3 = new JPanel();
+    P3.setBackground(new Color(0x97DFC6));
     P3.add(l3);
     P3.add(t3);
     
    
     //on créé les différents composant du pannel
     l4 = new JLabel("Quel est votre mot de passe ?");
-    t4 = new JTextField(20);
-    b4 = new JButton("Enregistrer"); 
+    t4 = new JPasswordField(15);
+    b4 = new JButton("Connexion"); 
     
     //On les ajoute au pannel
     P4 = new JPanel();
+    P4.setBackground(new Color(0x97DFC6));
+    P5 = new JPanel();
+    P5.setBackground(new Color(0x97DFC6));
+    Ptitre = new JPanel();
+    Ptitre.setBackground(new Color(0x97DFC6));
     P4.add(l4);
     P4.add(t4);
-    P4.add(b4);
+    P5.add(b4);
+    titre = new JLabel("ACCUEIL");
+    
+    Ptitre.add(titre);
     
     
     //on créé la box
     Box1 = Box.createVerticalBox();
     //on ajoute les différents Pannels dans la box
+    Box1.add(Ptitre);
     Box1.add(P2);
     Box1.add(P3);
     Box1.add(P4);
+    Box1.add(P5);
     
     //on ajoute la box dans le premier pannel
     P1.add(Box1);
@@ -115,12 +120,12 @@ public class FenetreAcceuil extends JFrame implements ActionListener
          login= t3.getText();
          passw= t4.getText();
          
-         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         
          try {
              conni = new Connexion (nom_data,login, passw);
+             
          } catch (SQLException ex) {
              Logger.getLogger(FenetreAcceuil.class.getName()).log(Level.SEVERE, null, ex);
+             
          } catch (ClassNotFoundException ex) {
              Logger.getLogger(FenetreAcceuil.class.getName()).log(Level.SEVERE, null, ex);
          }
